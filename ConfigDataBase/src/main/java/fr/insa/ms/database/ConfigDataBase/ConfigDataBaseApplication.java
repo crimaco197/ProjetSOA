601f613010556cb06d6c7c9897214acef5e50b83
“Crimaco197” <crmartinezco@unal.edu.co>
2024-11-21 05:19:23 +0100

    finished TD3 - connecting DB

---
+++ ConfigDataBase/src/main/java/fr/insa/ms/database/ConfigDataBase/ConfigDataBaseApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.database.ConfigDataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDataBaseApplication.class, args);
+	}
+
+}
---
+++ ConfigDataBase/src/test/java/fr/insa/ms/database/ConfigDataBase/ConfigDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.database.ConfigDataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
